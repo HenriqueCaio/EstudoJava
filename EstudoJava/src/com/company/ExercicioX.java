312a160faab07772b1e9cebada9ab75c4c8c70b6
Caio Henrique <henriquecaiofs@gmail.com>
2020-08-04 16:25:23 -0300

    imprimir todos os numeros de 150 a 300

---
+++ EstudoJava/src/com/company/ExercicioX.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class ExercicioX {
+
+    public static void main(String[] args) {
+        for (int part = 150; part <= 300; part = part + 1) {
+            System.out.println(part);
+        }
+    }
+}
