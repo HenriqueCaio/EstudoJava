7bd680e81e0a2bf422265a859731464aafd58d50
Caio Henrique <henriquecaiofs@gmail.com>
2020-08-04 18:14:44 -0300

    ExerciciosJava

--- EstudoJava/src/com/company/ExercicioX.java
+++ EstudoJava/src/com/company/ExercicioX.java
@@ -6,5 +6,26 @@ public class ExercicioX {
         for (int part = 150; part <= 300; part = part + 1) {
             System.out.println(part);
         }
+
+        int soma = 0;
+        for (int contador = 1; contador <= 1000; contador++) {
+            soma = soma + contador;
+
+            System.out.println("Contador: " + contador + " soma: " + soma);
+        }
+
+        for (int contador = 3; contador <= 100; contador++) {
+            if (contador % 3 == 0) {
+                System.out.println("multiplos de 3: " + contador);
+            }
+        }
+
+        int fatorial = 1; // inicializando variavel com o fatorial de 0
+        for (int contador = 1; contador <= 10; contador = contador + 1) {
+
+            fatorial = contador * fatorial;
+            System.out.println(fatorial);
+        }
     }
 }
+
